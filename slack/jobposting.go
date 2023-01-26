b590602c5aa0f5cb1c4a136f79f8597166f83a9f
CB <clint@getkolla.com>
2023-01-26 00:22:46 -0700

    parse body to json

--- slack/jobposting.go
+++ slack/jobposting.go
@@ -1,12 +1,17 @@
 package slack
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 
 	"encore.dev/rlog"
 )
+
+type SlackPayload struct {
+	Payload json.RawMessage `json:"payload"`
+}
 
 //encore:api public raw method=POST path=/slack/interactive
 func InteractiveRouter(w http.ResponseWriter, req *http.Request) {
@@ -17,5 +22,14 @@ func InteractiveRouter(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
-	rlog.Debug("Slack Interactive Webhook", "body", body)
+
+	// Create payload instance
+	var payload SlackPayload
+	err = json.Unmarshal(body, &payload)
+	if err != nil {
+		rlog.Error("Error unmarshalling payload", "payload", err)
+		http.Error(w, "can't unmarshal payload", http.StatusBadRequest)
+		return
+	}
+	rlog.Debug("Slack Interactive Webhook", "body_payload", payload.Payload)
 }
