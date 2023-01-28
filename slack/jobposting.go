4619009b9049680e6bc3bd9d931d77af7f6daa68
CB <clint@getkolla.com>
2023-01-27 21:36:28 -0700

    log the json

--- slack/jobposting.go
+++ slack/jobposting.go
@@ -1,13 +1,11 @@
 package slack
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 
 	"encore.dev/rlog"
-	"github.com/slack-go/slack"
 )
 
 //encore:api public raw method=POST path=/slack/interactive
@@ -25,26 +23,11 @@ func InteractiveRouter(w http.ResponseWriter, r *http.Request) {
 	}
 
 	jsonStr, err := url.QueryUnescape(string(body)[8:])
-	rlog.Info("Slack Interactive Webhook", "body_payload", jsonStr)
+
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	rlog.Info("Slack Interactive Webhook", "body_payload", jsonStr)
 
-	var message slack.InteractionCallback
-	if err := json.Unmarshal([]byte(jsonStr), &message); err != nil {
-		rlog.Error("[ERROR] Failed to decode json message from slack", "jsonstr", jsonStr)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	switch message.Type {
-	case slack.InteractionTypeShortcut:
-		message
-	default:
-		rlog.Error("[WARN] unknown message type: ", "meesage_type", message.Type)
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-
-	//rlog.Debug("Slack Interactive Webhook", "body_payload", message.Message)
 }
